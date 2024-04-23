f1ad02ded6c6c6c7e8761e555084db8a1436d1ff
Rifqi Hari Putranto <rifqiharip@gmail.com>
2024-04-23 11:07:51 +0700

    mycarprice Authentication

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,29 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { Observable, map } from "rxjs";
+
+interface ClassConstructor {
+    new (...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new  SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor{
+    constructor(private dto: any) {}
+    
+    intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+        // Run something before a request
+        
+
+        return handler.handle().pipe(
+            map((data: any) => {
+                // Run something before the response is sent out
+                return plainToClass(this.dto, data, {
+                    excludeExtraneousValues: true,
+                })
+            })
+        )
+    }
+}
